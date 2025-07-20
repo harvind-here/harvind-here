6db41f7c3e813a27f269787c59952894cb585907
harvind-here <harvindmukhal3@gmail.com>
2025-07-21 05:23:12 +0530

    Set basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/harvind-here',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
