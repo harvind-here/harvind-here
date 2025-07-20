02ce9dfc56c05731a0074315c77679809d532676
harvind-here <harvindmukhal3@gmail.com>
2025-07-21 05:05:21 +0530

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -27,5 +28,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-    
